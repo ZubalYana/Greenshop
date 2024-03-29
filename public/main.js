c55afc74121e74338fa6794daa4f5fe78e08c662
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-29 11:14:49 +0200

    fix the sum counting bug

--- public/main.js
+++ public/main.js
@@ -2,55 +2,55 @@ let db = [
     {
         id: 1,
         name: 'Barberton Daisy',
-        price: '$119.00',
+        price: 119,
         img: './imgs/plant 1.png',
     },
     {
         id: 2,
         name: 'Angel Wing Begonia',
-        price: '$169.00',
+        price: 169,
         img: './imgs/plant 2.png',
     },
     {
         id: 3,
         name: 'African Violet',
-        price: '$199.00',
+        price: 199,
         img: './imgs/plant 3.png',
     },
     {
         id: 4,
         name: 'Beach Spider Lily',
-        price: '$129.00',
+        price: 129,
         img: './imgs/plant 4.png',
     },
     {
         id: 5,
         name: 'Blushing Bromeliad',
-        price: '$139.00',
+        price: 139,
         img: './imgs/plant 5.png',
     },
     {
         id: 6,
         name: 'Aluminum Plant',
-        price: '$179.00',
+        price: 179,
         img: './imgs/plant 6.png',
     },
     {
         id: 7,
         name: 'Bird\'s Nest Fern',
-        price: '$99.00',
+        price: 99,
         img: './imgs/plant 7.png',
     },
     {
         id: 8,
         name: 'Broadleaf Lady Palm',
-        price: '$59.00',
+        price: 59,
         img: './imgs/plant 8.png',
     },
     {
         id: 9,
         name: 'Chinese Evergreen',
-        price: '$39.00',
+        price: 39,
         img: './imgs/plant 9.png',
     },
 ]
@@ -59,7 +59,7 @@ for(let el of db){
     <div class="goodsCard">
     <img src='${el.img}'>
     <h2>${el.name}</h2>
-    <h5>${el.price}</h5>
+    <h5>$${el.price}.00</h5>
     <button class="goodsCardBtn" id='buy${el.id}'><img class='goodsCardBtn_icon' src='./imgs/white cart.png'>Buy</button>
 </div>`)
 }
@@ -88,7 +88,7 @@ $('.cartContainer_cartElements').click(function(){
             <img src="${el.img}" alt="">
             <div class="popup_confirmSecrion_selectedGoods_Item_NamePrice">
                 <div class="popup_confirmSecrion_selectedGoods_Item_Name">${el.name}</div>
-                <div class="popup_confirmSecrion_selectedGoods_Item_Price">${el.price}</div>
+                <div class="popup_confirmSecrion_selectedGoods_Item_Price">$${el.price}.00</div>
             </div>
         </div>`
         )
